2ec3f7eb8e31d445e32756e13ff1bdb057cb9ca3
HIRO <hirotaka5315@gmail.com>
2020-03-07 02:40:59 +0900

    メールアドレス修正

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="text-white py-3 text-center">
     <p class="px-5">山県市総合体育館(岐阜県山県市高木1675)<br>
-      Skegon8808@gamail.com<br>
+      skegon8808@gmail.com<br>
       090-9338-5595
     </p>
     <p class="mb-0 text-center">Copyright © 2020 Skegon Dance Studio All Right Reserved.</p>
