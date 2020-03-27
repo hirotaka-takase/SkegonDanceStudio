31885786d7d4941a6786b1f0975f7b10dbdee4f3
HIRO <hirotaka5315@gmail.com>
2020-03-27 09:21:25 +0900

    padding調節

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
-<footer class="text-white py-3 text-center">
-    <p class="px-5">山県市総合体育館(岐阜県山県市高木1675)<br>
+<footer class="text-white py-2 text-center">
+    <p>山県市総合体育館(岐阜県山県市高木1675)<br>
       skegon8808@gmail.com<br>
       090-9338-5595
     </p>
-    <p class="mb-0 text-center">Copyright © 2020 Skegon Dance Studio All Right Reserved.</p>
+    <p class="mb-0 small text-center">Copyright © 2020 Skegon Dance Studio All Right Reserved.</p>
 </footer>  
 </body>   
 </html>
