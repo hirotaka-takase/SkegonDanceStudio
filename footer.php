26a8644aad493802c9eb785f469ce868af004b9e
HIRO <hirotaka5315@gmail.com>
2020-04-01 10:53:30 +0900

    instaリンク生成

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <footer class="text-white py-2 text-center">
     <p>山県市総合体育館(岐阜県山県市高木1675)<br>
       skegon8808@gmail.com<br>
-      090-9338-5595
+      090-9338-5595<br>
+      instagram: <a href="https://www.instagram.com/skegon_dance_studio/" target="_blank" rel=”noopener”>@skegondancestudio</a>
     </p>
     <p class="mb-0 small text-center">Copyright © 2020 Skegon Dance Studio All Right Reserved.</p>
 </footer>  
